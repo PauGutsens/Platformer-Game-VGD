39d7b409a33d00f901c18d5daca6533352bd0e86
ZReiNa <saucxz@gmail.com>
2024-10-28 20:20:34 +0100

    Change in debug key
    changed collisions showing in debug mode from f1 to f9. Also changed so that collisions are not visible by default.

--- src/Physics.cpp
+++ src/Physics.cpp
@@ -14,7 +14,7 @@ Physics::Physics() : Module()
 {
 	// Initialise all the internal class variables, at least to NULL pointer
 	world = NULL;
-	debug = true;
+	debug = false;
 }
 
 // Destructor
@@ -217,7 +217,7 @@ bool Physics::PostUpdate()
 	bool ret = true;
 
 	// Activate or deactivate debug mode
-	if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_F1) == KEY_DOWN)
+	if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_F9) == KEY_DOWN)
 		debug = !debug;
 	
 	//  Iterate all objects in the world and draw the bodies
