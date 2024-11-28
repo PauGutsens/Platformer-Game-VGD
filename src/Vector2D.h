53c16d56b94a028f698a79b6e596f01c88aaad93
PauGutsens <159762692+PauGutsens@users.noreply.github.com>
2024-11-28 22:19:46 +0100

    Update Vector2D.h

--- src/Vector2D.h
+++ src/Vector2D.h
@@ -21,7 +21,13 @@ public:
     Vector2D operator-(const Vector2D& other) const;
     Vector2D operator*(float scalar) const;
     Vector2D operator/(float scalar) const;
+    bool operator==(const Vector2D& other) const;
+bool operator!=(const Vector2D& other) const;
+bool operator<(const Vector2D& other) const;
 
+float distanceMahattan(const Vector2D& other) const;
+float distanceEuclidean(const Vector2D& other) const;
+float distanceSquared(const Vector2D& other) const;
     // Output stream operator
     friend std::ostream& operator<<(std::ostream& os, const Vector2D& vec);
 
