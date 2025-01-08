982f4ffeb5894d43e2eb93d68ad6f2286ce54f41
PauGutsens <159762692+PauGutsens@users.noreply.github.com>
2025-01-08 12:59:05 +0100

    Variables para el pathfinding del enemigo

--- src/Enemy.h
+++ src/Enemy.h
@@ -10,38 +10,31 @@ struct SDL_Texture;
 class Enemy : public Entity
 {
 public:
+    Enemy();
+    virtual ~Enemy();
 
-	Enemy();
-	virtual ~Enemy();
+    bool Awake();
+    bool Start();
+    bool Update(float dt);
+    bool CleanUp();
 
-	bool Awake();
+    void SetParameters(pugi::xml_node parameters) {
+        this->parameters = parameters;
+    }
 
-	bool Start();
+    void SetPosition(Vector2D pos);
+    Vector2D GetPosition();
+    void ResetPath();
 
-	bool Update(float dt);
-
-	bool CleanUp();
-
-	void SetParameters(pugi::xml_node parameters) {
-		this->parameters = parameters;
-	}
-
-	void SetPosition(Vector2D pos);
-
-	Vector2D GetPosition();
-
-	void ResetPath();
-
-public:
 
 private:
-
-	SDL_Texture* texture;
-	const char* texturePath;
-	int texW, texH;
-	pugi::xml_node parameters;
-	Animation* currentAnimation = nullptr;
-	Animation idle;
-	PhysBody* pbody;
-	Pathfinding* pathfinding;
+    SDL_Texture* texture;
+    const char* texturePath;
+    int texW, texH;
+    pugi::xml_node parameters;
+    Animation* currentAnimation = nullptr;
+    Animation idle;
+    PhysBody* pbody;
+    Pathfinding* pathfinding;
+    float speed = 3.0f; // Enemy's velocity
 };
